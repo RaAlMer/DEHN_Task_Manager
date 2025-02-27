956617fb5fcb64af431b311eab42e96df7ad26c9
Raúl <raul.alonso.merino@gmail.com>
2025-02-27 02:44:44 +0100

    Make table responsive

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -61,61 +61,63 @@ const sortBy = (key: keyof (typeof taskStore.tasks)[0]) => {
 <template>
   <div>
     <!-- Tabular Task List -->
-    <table
-      v-if="0 < taskStore.tasks.length"
-      class="w-full border-collapse border border-gray-200 mt-4"
-    >
-      <thead>
-        <tr class="bg-gray-100">
-          <th class="border p-2 cursor-pointer" @click="sortBy('id')">
-            ID
-            <span v-if="sortKey === 'id'">{{
-              sortOrder === 'asc' ? '↑' : sortOrder === 'desc' ? '↓' : ''
-            }}</span>
-          </th>
-          <th class="border p-2 cursor-pointer" @click="sortBy('title')">
-            Title
-            <span v-if="sortKey === 'title'">{{
-              sortOrder === 'asc' ? '↑' : sortOrder === 'desc' ? '↓' : ''
-            }}</span>
-          </th>
-          <th class="border p-2">Description</th>
-          <th class="border p-2 cursor-pointer" @click="sortBy('dueDate')">
-            Due Date
-            <span v-if="sortKey === 'dueDate'">{{
-              sortOrder === 'asc' ? '↑' : sortOrder === 'desc' ? '↓' : ''
-            }}</span>
-          </th>
-          <th class="border p-2 cursor-pointer" @click="sortBy('completed')">
-            Status
-            <span v-if="sortKey === 'completed'">{{
-              sortOrder === 'asc' ? '↑' : sortOrder === 'desc' ? '↓' : ''
-            }}</span>
-          </th>
-          <th class="border p-2">Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="task in sortedTasks" :key="task.id">
-          <td class="border p-2">{{ task.id }}</td>
-          <td class="border p-2 title">{{ task.title }}</td>
-          <td class="border p-2 description">{{ task.description }}</td>
-          <td class="border p-2">{{ task.dueDate }}</td>
-          <td class="border p-2 status">
-            <span :class="task.completed ? 'text-green-600' : 'text-red-600'" class="font-bold">
-              {{ task.completed ? 'Completed' : 'Pending' }}
-            </span>
-          </td>
-          <td class="border p-2">
-            <button @click="taskStore.toggleTaskCompletion(task.id)" class="mr-2">
-              Toggle Status
-            </button>
-            <button @click="openEditModal(task.id)" class="mr-2 action">Update</button>
-            <button @click="taskStore.deleteTask(task.id)" class="action">Delete</button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="overflow-x-auto">
+      <table
+        v-if="0 < taskStore.tasks.length"
+        class="w-full border-collapse border border-gray-200 mt-4 responsive-table"
+      >
+        <thead>
+          <tr class="bg-gray-100">
+            <th class="border p-2 cursor-pointer" @click="sortBy('id')">
+              ID
+              <span v-if="sortKey === 'id'">{{
+                sortOrder === 'asc' ? '↑' : sortOrder === 'desc' ? '↓' : ''
+              }}</span>
+            </th>
+            <th class="border p-2 cursor-pointer" @click="sortBy('title')">
+              Title
+              <span v-if="sortKey === 'title'">{{
+                sortOrder === 'asc' ? '↑' : sortOrder === 'desc' ? '↓' : ''
+              }}</span>
+            </th>
+            <th class="border p-2">Description</th>
+            <th class="border p-2 cursor-pointer" @click="sortBy('dueDate')">
+              Due Date
+              <span v-if="sortKey === 'dueDate'">{{
+                sortOrder === 'asc' ? '↑' : sortOrder === 'desc' ? '↓' : ''
+              }}</span>
+            </th>
+            <th class="border p-2 cursor-pointer" @click="sortBy('completed')">
+              Status
+              <span v-if="sortKey === 'completed'">{{
+                sortOrder === 'asc' ? '↑' : sortOrder === 'desc' ? '↓' : ''
+              }}</span>
+            </th>
+            <th class="border p-2">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="task in sortedTasks" :key="task.id">
+            <td class="border p-2">{{ task.id }}</td>
+            <td class="border p-2 title">{{ task.title }}</td>
+            <td class="border p-2 description">{{ task.description }}</td>
+            <td class="border p-2">{{ task.dueDate }}</td>
+            <td class="border p-2 status">
+              <span :class="task.completed ? 'text-green-600' : 'text-red-600'" class="font-bold">
+                {{ task.completed ? 'Completed' : 'Pending' }}
+              </span>
+            </td>
+            <td class="border p-2">
+              <button @click="taskStore.toggleTaskCompletion(task.id)" class="mr-2">
+                Toggle Status
+              </button>
+              <button @click="openEditModal(task.id)" class="mr-2 action">Update</button>
+              <button @click="taskStore.deleteTask(task.id)" class="action">Delete</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
 
     <!-- Edit Task Modal -->
     <EditTaskModal v-if="showModal" :taskId="selectedTaskId" @close="closeModal" />
